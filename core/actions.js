2e49e6caaae2131e6601841bce804dfafef243db
AhnafMohsieu <mahinahmed923@gmail.com>
2026-08-02 01:54:34 +0600

    fix: completeChallenge scope issue in App object

--- core/actions.js
+++ core/actions.js
@@ -2154,7 +2154,7 @@ Object.keys(NEW_POOLS).forEach(k => {
       detail: (id) => toast('ℹ️', DETAILS[id]||'Voluntary Prayer', false, 4000),
       tip: (id) => toast('💡', TIPS[id]||'A beautiful deed!', false, 4000),
       dismissTip: () => { S.tdismiss=true; saveState(); renderTip(); },
-      toggleQuest, completeChallenge, addGratitude, toggleFasting, setCharityGoals,
+      toggleQuest, completeChallenge: window.completeChallenge, addGratitude, toggleFasting, setCharityGoals,
       addMemorization, toggleMorning, toggleEvening, switchUser, logout, resetAll,
       manualRefresh: manualRefreshContent, switchCategory, activateTab,
       claimBonus, tapDhikr, resetDhikr, nextDhikr,
